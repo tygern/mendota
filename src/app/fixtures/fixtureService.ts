d33e893be31677192d5303fd4e1c2fc9909607e1
Tyson Gern <tygern@gmail.com>
2016-04-17 20:11:48 +0100

    Use BehaviorSubject

--- src/app/fixtures/fixtureService.ts
+++ src/app/fixtures/fixtureService.ts
@@ -1,6 +1,7 @@
-import {Http, Headers} from "angular2/http";
+import {Http, Headers, Response} from "angular2/http";
 import {Injectable} from "angular2/core";
 import {Observable} from "rxjs/Observable";
+import {BehaviorSubject} from "rxjs/subject/BehaviorSubject";
 import "rxjs/Rx";
 import {Fixture} from "./fixture";
 import {Team} from "./team";
@@ -9,28 +10,38 @@ import {FixtureProvider} from "./fixtureProvider";
 @Injectable()
 export class FixtureService implements FixtureProvider {
 
+    private subject:BehaviorSubject<Fixture[]> = new BehaviorSubject([]);
+
     constructor(private _http:Http) {
+        this.updateList();
     }
 
     fetchList():Observable<Fixture[]> {
+        return this.subject.asObservable();
+    }
+
+    private updateList() {
         let headers = new Headers({'X-Auth-Token': 'b61179888f4547ddb9b5fa62d2ed4700'});
 
-        return this._http.get(
+        this._http.get(
             "http://api.football-data.org/v1/soccerseasons/398/fixtures?timeFrame=n7",
             {headers}
             )
-            .map((response) => {
-                return response.json().fixtures.map((fixture) => {
-                    let homeTeam = new Team(fixture.homeTeamName);
-                    let awayTeam = new Team(fixture.awayTeamName);
+            .map(this.responseToFixtureList)
+            .subscribe((fixtureList) => this.subject.next(fixtureList));
+    }
 
-                    return new Fixture(
-                        homeTeam,
-                        awayTeam,
-                        fixture.result.goalsHomeTeam,
-                        fixture.result.goalsAwayTeam
-                    );
-                });
-            })
+    private responseToFixtureList(response:Response) {
+        return response.json().fixtures.map((fixture) => {
+            let homeTeam = new Team(fixture.homeTeamName);
+            let awayTeam = new Team(fixture.awayTeamName);
+
+            return new Fixture(
+                homeTeam,
+                awayTeam,
+                fixture.result.goalsHomeTeam,
+                fixture.result.goalsAwayTeam
+            );
+        });
     }
 }
--- src/test/fixtureService.test.ts
+++ src/test/fixtureService.test.ts
@@ -7,56 +7,60 @@ import {FixtureService} from "../app/fixtures/fixtureService";
 
 describe('FixtureService', () => {
 
+    let mockBackend:MockBackend;
+
     beforeEachProviders(() => {
+        mockBackend = new MockBackend();
+
+        mockBackend.connections.subscribe(connection => {
+            connection.mockRespond(new Response(new ResponseOptions({
+                status: 200,
+                body: {
+                    "fixtures": [{
+                        "_links": {
+                            "self": {"href": "http://api.football-data.org/v1/fixtureList/146780"},
+                            "soccerseason": {"href": "http://api.football-data.org/v1/soccerseasons/398"},
+                            "homeTeam": {"href": "http://api.football-data.org/v1/teams/68"},
+                            "awayTeam": {"href": "http://api.football-data.org/v1/teams/71"}
+                        },
+                        "date": "2016-04-16T11:45:00Z",
+                        "status": "FINISHED",
+                        "matchday": 34,
+                        "homeTeamName": "Norwich City FC",
+                        "awayTeamName": "Sunderland AFC",
+                        "result": {"goalsHomeTeam": 0, "goalsAwayTeam": 3}
+                    }, {
+                        "_links": {
+                            "self": {"href": "http://api.football-data.org/v1/fixtureList/146782"},
+                            "soccerseason": {"href": "http://api.football-data.org/v1/soccerseasons/398"},
+                            "homeTeam": {"href": "http://api.football-data.org/v1/teams/74"},
+                            "awayTeam": {"href": "http://api.football-data.org/v1/teams/346"}
+                        },
+                        "date": "2016-04-16T14:00:00Z",
+                        "status": "FINISHED",
+                        "matchday": 34,
+                        "homeTeamName": "West Bromwich Albion FC",
+                        "awayTeamName": "Watford FC",
+                        "result": {"goalsHomeTeam": 0, "goalsAwayTeam": 1}
+                    }]
+                }
+            })));
+        });
+
         return [
             HTTP_PROVIDERS,
-            provide(XHRBackend, {useClass: MockBackend}),
+            provide(XHRBackend, {useValue: mockBackend}),
             FixtureService
         ];
     });
 
-    it('logs in', injectAsync([XHRBackend, FixtureService], (mockBackend, fixtureService) => {
+    it('logs in', injectAsync([FixtureService], (fixtureService) => {
         return new Promise(
             (resolve) => {
-
                 mockBackend.connections.subscribe(connection => {
-
                     expect(connection.request.url.toString()).toEqual("http://api.football-data.org/v1/soccerseasons/398/fixtures?timeFrame=n7");
                     expect(connection.request.method).toEqual(RequestMethod.Get);
                     expect(connection.request.headers.get("X-Auth-Token")).toEqual("b61179888f4547ddb9b5fa62d2ed4700");
-
-                    connection.mockRespond(new Response(new ResponseOptions({
-                        status: 200,
-                        body: {
-                            "fixtures": [{
-                                "_links": {
-                                    "self": {"href": "http://api.football-data.org/v1/fixtureList/146780"},
-                                    "soccerseason": {"href": "http://api.football-data.org/v1/soccerseasons/398"},
-                                    "homeTeam": {"href": "http://api.football-data.org/v1/teams/68"},
-                                    "awayTeam": {"href": "http://api.football-data.org/v1/teams/71"}
-                                },
-                                "date": "2016-04-16T11:45:00Z",
-                                "status": "FINISHED",
-                                "matchday": 34,
-                                "homeTeamName": "Norwich City FC",
-                                "awayTeamName": "Sunderland AFC",
-                                "result": {"goalsHomeTeam": 0, "goalsAwayTeam": 3}
-                            }, {
-                                "_links": {
-                                    "self": {"href": "http://api.football-data.org/v1/fixtureList/146782"},
-                                    "soccerseason": {"href": "http://api.football-data.org/v1/soccerseasons/398"},
-                                    "homeTeam": {"href": "http://api.football-data.org/v1/teams/74"},
-                                    "awayTeam": {"href": "http://api.football-data.org/v1/teams/346"}
-                                },
-                                "date": "2016-04-16T14:00:00Z",
-                                "status": "FINISHED",
-                                "matchday": 34,
-                                "homeTeamName": "West Bromwich Albion FC",
-                                "awayTeamName": "Watford FC",
-                                "result": {"goalsHomeTeam": 0, "goalsAwayTeam": 1}
-                            }]
-                        }
-                    })));
                 });
 
                 fixtureService.fetchList().subscribe(
