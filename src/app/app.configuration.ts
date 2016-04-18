e0e5522fc16bba98ea34e66f6356725948b18af1
Tyson Gern <tygern@gmail.com>
2016-04-18 21:06:18 +0100

    Allow for list of service providers in config

--- src/app/app.configuration.ts
+++ src/app/app.configuration.ts
@@ -3,4 +3,6 @@ import {provide} from "angular2/core";
 import {FixtureService} from "./fixtures/fixtureService";
 import {FixtureProviderToken} from "./fixtures/fixtureProvider";
 
-export const SERVICE_PROVIDERS = provide(FixtureProviderToken, {useClass: FixtureService});
+export const SINGLETON_SERVICE_PROVIDERS = [
+    provide(FixtureProviderToken, {useClass: FixtureService})
+];
--- src/app/main.ts
+++ src/app/main.ts
@@ -6,10 +6,10 @@ import {ROUTER_PROVIDERS} from "angular2/router";
 import 'rxjs/Rx'
 
 import {AppComponent} from "./app.component";
-import {SERVICE_PROVIDERS} from "./app.configuration";
+import {SINGLETON_SERVICE_PROVIDERS} from "./app.configuration";
 
 bootstrap(AppComponent, [
-    SERVICE_PROVIDERS,
+    SINGLETON_SERVICE_PROVIDERS,
     HTTP_PROVIDERS,
     ROUTER_PROVIDERS
 ]);
