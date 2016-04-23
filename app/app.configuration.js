893853c87e62d24ef7d6c647d39779387f4b1670
Tyson Gern <tygern@gmail.com>
2016-04-23 10:08:14 +0100

    Use safe provider

--- app/app.configuration.js
+++ app/app.configuration.js
@@ -1,20 +1,25 @@
-System.register(["angular2/core", "./fixtures/fixtureService", "./fixtures/fixtureProvider"], function(exports_1) {
-    var core_1, fixtureService_1, fixtureProvider_1;
+System.register(["angular2/core", "./fixtures/fixtureProvider", "./fixtures/fixture"], function(exports_1) {
+    var core_1, fixtureProvider_1, fixture_1;
     var SINGLETON_SERVICE_PROVIDERS;
+    function safeProvider(token) {
+        return function setClass(Klass) {
+            return core_1.provide(token, { useClass: Klass });
+        };
+    }
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (fixtureService_1_1) {
-                fixtureService_1 = fixtureService_1_1;
-            },
             function (fixtureProvider_1_1) {
                 fixtureProvider_1 = fixtureProvider_1_1;
+            },
+            function (fixture_1_1) {
+                fixture_1 = fixture_1_1;
             }],
         execute: function() {
             exports_1("SINGLETON_SERVICE_PROVIDERS", SINGLETON_SERVICE_PROVIDERS = [
-                core_1.provide(fixtureProvider_1.FixtureProviderToken, { useClass: fixtureService_1.FixtureService })
+                safeProvider(fixtureProvider_1.FixtureProviderToken)(fixture_1.Fixture)
             ]);
         }
     }
