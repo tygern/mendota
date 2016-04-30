aaeb84ed722c720d987050c5ba98d48e20e1e78c
Tyson Gern <tygern@gmail.com>
2016-04-30 12:51:46 +0100

    Update angular2-safe-provider version

--- src/app/app.configuration.ts
+++ src/app/app.configuration.ts
@@ -1,8 +1,8 @@
-import {safeProvider} from "angular2-safe-provider"
+import {safeProvide} from "angular2-safe-provider"
 
 import {FixtureService} from "./fixtures/fixtureService";
 import {FixtureProviderToken, FixtureProvider} from "./fixtures/fixtureProvider";
 
 export const SINGLETON_SERVICE_PROVIDERS = [
-    safeProvider<FixtureProvider>(FixtureProviderToken)<FixtureService>(FixtureService)
+    safeProvide<FixtureProvider>(FixtureProviderToken)<FixtureService>(FixtureService)
 ];
